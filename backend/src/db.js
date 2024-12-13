8535bf2fa2a1b1f74081dbad88fe2dd96d68ec21
santiagocossi <131680532+santiagocossi@users.noreply.github.com>
2024-12-13 08:34:59 -0400

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/FinalCossio'); 
+    await mongoose.connect('mongodb+srv://Santiago:12345678910@cluster0.qku3i.mongodb.net/FinalCossio?retryWrites=true&w=majority'); 
     console.log('>>> DB conectada');
   } catch (error) {
     console.error('Error al conectar la base de datos:', error);
